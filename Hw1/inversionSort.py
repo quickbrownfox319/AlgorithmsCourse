21d32b33d63f92e7259c2601047f2a672284c4a7
Daniel <quickbrownfox319@gmail.com>
2015-01-29 00:27:27 -0500

    updated even case

--- Hw1/inversionSort.py
+++ Hw1/inversionSort.py
@@ -1,8 +1,10 @@
 #merge sort
 
-inputList = []
-file = open('filename.txt', 'r')
-inputList.append(file.readline())
+#inputList = []
+#file = open('filename.txt', 'r')
+#inputList.append(file.readline())
+
+inputList = [1,2,3,4]
 
 def splitList(inputList):
 	#base case of 1 element
@@ -11,4 +13,7 @@ def splitList(inputList):
 
 	#even list case
 	if len(inputList) % 2 == 0:
-		leftList[i for i in inputList[:len(inputList)/2]
+		leftList = [inputList[i] for i in range(0,len(inputList)/2)]
+		rightList = [inputList[j] for j in range(len(inputList)/2,len(inputList))]
+		print leftList
+		print rightList
