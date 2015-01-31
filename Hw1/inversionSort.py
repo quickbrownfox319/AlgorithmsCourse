3e6e6f5b8a9b79c0453a22d463f4b25d50cecb74
Daniel <quickbrownfox319@gmail.com>
2015-01-30 21:47:10 -0500

    fixed possible bug by giving splitList() recursion variable names finalLeft and finalRight and return 'result'

--- Hw1/inversionSort.py
+++ Hw1/inversionSort.py
@@ -4,7 +4,7 @@
 #file = open('filename.txt', 'r')
 #inputList.append(file.readline())
 
-test = [6,5,4,3,2,1]
+test = [6,5,4,3]
 
 def splitList(inputList):
         length = len(inputList)
@@ -15,30 +15,34 @@ def splitList(inputList):
 
         leftList = [inputList[i] for i in range(0,length/2)]
         rightList = [inputList[j] for j in range(length/2,length)]
-        splitList(leftList)
-        print "Left list is {}\n".format(leftList)
-        splitList(rightList)
-        print "Right list is{}\n".format(rightList)
+        finalLeft = splitList(leftList)
+        print "Left list is {}\n".format(finalLeft)
+        finalRight = splitList(rightList)
+        print "Right list is{}\n".format(finalRight)
         
         #merge
-        if (len(leftList) and len(rightList)) == 1:
-                if leftList[0] < rightList[0]:
-                        result.append(leftList[0])
-                        result.append(rightList[0])
+        if (len(finalLeft) and len(finalRight)) == 1:
+                if finalLeft[0] < finalRight[0]:
+                        result.append(finalLeft[0])
+                        result.append(finalRight[0])
+                        return result
                 else:
                         #HERE BE INVERSIONS
-                        result.append(rightList[0])
-                        result.append(leftList[0])
-#        elif len(leftList) != len(rightList):
+                        result.append(finalRight[0])
+                        result.append(finalLeft[0])
+                        return result
+#        elif len(finalLeft) != len(finalRight):
         else:
-                for i in leftList:
-                        if leftList[i] < rightList[i]:
-                                result.append(leftList[i])
-                                result.append(rightList[i])
+                for i in finalLeft:
+                        if finalLeft[i] < finalRight[i]:
+                                result.append(finalLeft[i])
+                                result.append(finalRight[i])
+                                return result
                         else:
                                 #HERE BE INVERSIONS
-                                result.append(rightList[i])
-                                result.append(leftList[i])
+                                result.append(finalRight[i])
+                                result.append(finalLeft[i])
+                                return result
         print "Merged list {}\n".format(result)
 
 splitList(test)
